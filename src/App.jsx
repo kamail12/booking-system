5f75de4fd5d02eb9e195e5419bec0583ecc6cfa3
kamail12 <kamiloxen1@gmail.com>
2024-06-05 03:48:55 +0200

    add: AppLayout and sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Settings from './pages/Settings';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
 
 const App = () => {
   const basename = '/booking-system/';
@@ -19,13 +20,15 @@ const App = () => {
       <GlobalStyles />
       <BrowserRouter basename={basename}>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
